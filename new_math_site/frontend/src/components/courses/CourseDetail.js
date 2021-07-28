4ca1a36f58f2a78593aaef52b38f7530f1377ed0
Alexa201228 <aleksandra.grid2012@gmail.com>
2021-07-28 21:35:25 +0300

    fix trouble with authentication (again)

--- new_math_site/frontend/src/components/courses/CourseDetail.js
+++ new_math_site/frontend/src/components/courses/CourseDetail.js
@@ -1,6 +1,7 @@
 import React, { Fragment, Component, useEffect, useState } from 'react';
 import { courseDetails } from '../../actions/courses';
-import { shallowEqual, useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import Button from '@material-ui/core/Button';
 import { useParams } from 'react-router';
 
 
@@ -9,13 +10,21 @@ export function CourseDetail(){
     let { slug } = useParams()
     const dispatch = useDispatch()
     const {courses} = useSelector(state => state.courses)
+    const { isAuthenticated } = useSelector(state => state.auth)
     useEffect(() => {
         dispatch(courseDetails(slug));
     }, []);
     
         return(
             <Fragment>
-                  <h3>{courses.title}</h3> 
+                  <h3>{courses.title}</h3>
+                    {isAuthenticated ? 
+                    <Button
+                    type='submit'
+                    color='primary'
+                    variant="contained">
+                    Enroll course
+                </Button>: null}
             </Fragment>
         )
     }
