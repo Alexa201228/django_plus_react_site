44717808596b1841f4fec7f2513fa8472048f65d
Alexa201228 <aleksandra.grid2012@gmail.com>
2021-07-29 20:45:33 +0300

    finally add enroll course view (by id)

--- new_math_site/courses/api/views.py
+++ new_math_site/courses/api/views.py
@@ -1,11 +1,11 @@
 from rest_framework import generics
 from rest_framework import viewsets
 from rest_framework import permissions
-from rest_framework_simplejwt.authentication import JWTTokenUserAuthentication
-from rest_framework.decorators import action, authentication_classes, permission_classes
+from rest_framework.decorators import action
 from rest_framework_simplejwt.exceptions import AuthenticationFailed
 from rest_framework.response import Response
 from rest_framework import status
+
 from ..models import Course
 from .serializers import *
 
@@ -15,6 +15,31 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
     permission_classes = [permissions.AllowAny]
+
+    """
+    Custom function to register user on course
+    from course detail page
+    """
+    @action(
+        methods=['post'],
+        detail=True,
+        permission_classes=[permissions.IsAuthenticated,])
+    def enroll(self, request, *args, **kwargs):
+        try:
+            course = self.get_object()
+            course.students_on_course.add(request.user)
+            request.user.student_courses.add(course)
+            return Response(status=status.HTTP_202_ACCEPTED)
+        except AuthenticationFailed as auth_fail:
+            return Response(
+                {'error': auth_fail.detail},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+        except Exception as e:
+            return Response(
+                {'error': e},
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
 
 class CourseListView(generics.ListAPIView):
@@ -27,33 +52,3 @@ class CourseDetailView(generics.RetrieveAPIView):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
     permission_classes = [permissions.AllowAny]
-
-    """
-    Custom function to register user on course
-    from course detail page
-    """
-
-    @action(
-        methods=['post'],
-        detail=True,
-        authentication_classes=[JWTTokenUserAuthentication,],
-        permission_classes=[permissions.IsAuthenticated])
-    def enroll(self, request, *args, **kwargs):
-        try:
-            course = self.get_object()
-            course.students_on_course.add(request.user)
-        except AuthenticationFailed as auth_fail:
-            return Response(
-                status=status.HTTP_401_UNAUTHORIZED,
-                context={
-                    'error': auth_fail.detail
-                }
-            )
-        except Exception as e:
-            return Response(
-                status=status.HTTP_400_BAD_REQUEST,
-                context={
-                    'error': e
-                }
-            )
-
