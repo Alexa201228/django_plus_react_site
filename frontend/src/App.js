29ea6c04394dff770201eabe6fd967e241efe682
Alexa201228 <aleksandra.gr2012@gmail.com>
2022-06-16 20:23:16 +0300

    attempt to fix outlet component issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -69,7 +69,7 @@ export function App() {
         <Provider store={store}>
             <PersistGate persistor={persistor}>
                 <AlertProvider template={AlertTemplate} {...alertOptions}>
-                    <BrowserRouter basename={process.env.PUBLIC_URL}>
+                    <BrowserRouter>
                         <Fragment>
                             <Header/>
                             <Alerts/>
