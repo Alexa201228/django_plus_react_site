aa87cce4cd88b20e351f8da523269074e844eb9a
Alexa201228 <aleksandra.gr2012@gmail.com>
2022-06-18 17:47:31 +0300

    attempt to fix outlet component issue

--- new_math_site/new_math_site/settings.py
+++ new_math_site/new_math_site/settings.py
@@ -262,8 +262,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 # Place static in the same location as webpack build files
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR.parent, 'build/static/')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR.parent, 'build/static')]
+STATIC_ROOT = os.path.join(BASE_DIR.parent, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
