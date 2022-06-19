a69289ccede9226cc82037b59b0bb2e31c5c7bff
Alexa201228 <aleksandra.gr2012@gmail.com>
2022-06-19 16:10:18 +0300

    attempt to fix outlet component issue

--- new_math_site/settings.py
+++ new_math_site/settings.py
@@ -18,7 +18,7 @@ import django_heroku
 
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
