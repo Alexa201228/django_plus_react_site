fa6c1fbd19f7aeabb11d96bdb98b396b0247ff5d
Alexa201228 <aleksandra.gr2012@gmail.com>
2022-06-19 17:23:05 +0300

    attempt to deploy code on another server

--- new_math_site/settings.py
+++ new_math_site/settings.py
@@ -261,8 +261,8 @@ DEFAULT_FROM_EMAIL = 'noreply@<kubstu>.com'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 # Place static in the same location as webpack build files
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'build/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'build/static')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, '')]
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
