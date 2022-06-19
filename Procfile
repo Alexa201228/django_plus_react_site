5a44a0011e33cea079dbc58d21a57ae0b03f6000
Alexa201228 <aleksandra.gr2012@gmail.com>
2022-06-19 13:10:09 +0300

    attempt to fix outlet component issue

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 release: python manage.py migrate
-web: gunicorn --pythonpath new_math_site.wsgi:application --log-file - --log-level debug && npm run build
+web: gunicorn --pythonpath new_math_site new_math_site.wsgi:application --log-file - --log-level debug && npm run build
--- new_math_site/settings.py
+++ new_math_site/settings.py
@@ -104,7 +104,7 @@ WSGI_APPLICATION = 'new_math_site.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
+        'NAME': os.getenv('POSTGRES_DB_NAME'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': 'localhost',
