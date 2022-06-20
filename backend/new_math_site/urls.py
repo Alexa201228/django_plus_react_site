963e453024922fa1a1598beae251a074d8a5e86e
Alexa201228 <aleksandra.gr2012@gmail.com>
2022-06-20 16:46:37 +0300

    attempt to deploy code on another server

--- backend/new_math_site/urls.py
+++ backend/new_math_site/urls.py
@@ -45,7 +45,7 @@ urlpatterns = [
     path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify', TokenVerifyView.as_view(), name='token_verify'),
-    path('admin/', admin.site.urls),
+    path('admin/web', admin.site.urls),
     path('api/', include('courses.api.urls', namespace='courses')),
     path('api/', include('accounts.api.urls', namespace='accounts')),
     path('api/', include('testsApp.api.urls', namespace='tests')),
@@ -54,5 +54,5 @@ urlpatterns = [
          cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc',
          cache_timeout=0), name='schema-redoc'),
-    re_path('.*', TemplateView.as_view(template_name='index.html')),
+    re_path('', TemplateView.as_view(template_name='index.html')),
 ]
