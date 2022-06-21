6d161c9ac0a7f6d575228f3f4a9a576a014c293a
Alexa201228 <aleksandra.gr2012@gmail.com>
2022-06-21 15:33:27 +0300

    attempt to deploy code on another server

--- frontend/src/helpers/requiredConst.js
+++ frontend/src/helpers/requiredConst.js
@@ -1,3 +1,3 @@
 export const EMAIL_TEXTFIELD_VALIDATOR = 'Пожалуйтса, введите действительный email адрес';
 export const REQUIRED_FIELD = 'Обязательное поле';
-export const API_PATH = window.location.href == 'http://localhost:3000' ? 'http://localhost:8000' : window.location.href;
+export const API_PATH = window.location.origin == 'http://localhost:3000' ? 'http://localhost:8000' : window.location.origin;
